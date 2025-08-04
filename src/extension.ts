9fe8cea90e270cd7996230e35793cf0d0b76ae74
Andrey Litvitski <andrey1010102008@gmail.com>
2025-08-04 20:01:38 +0300

    revert to no-web extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as crypto from 'crypto';
 
 export async function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('spotilyrics.showLyrics', async () => {
@@ -15,16 +16,28 @@ export async function activate(context: vscode.ExtensionContext) {
         const htmlUri = vscode.Uri.joinPath(context.extensionUri, 'media', 'logInTemplate.html');
         const bytes = await vscode.workspace.fs.readFile(htmlUri);
 
-        // Используем TextDecoder, чтобы получить строку из Uint8Array
         const html = new TextDecoder("utf-8").decode(bytes);
 
         const cssUri = panel.webview.asWebviewUri(
             vscode.Uri.joinPath(context.extensionUri, 'media', 'styles.css')
         );
 
-        const finalHtml = html.replace('styles.css', cssUri.toString());
+        panel.webview.html = html.replace('styles.css', cssUri.toString());
 
-        panel.webview.html = finalHtml;
+        panel.webview.onDidReceiveMessage(message => {
+            if (message.command === 'signInClicked') {
+                const codeVerifier = message.message;
+
+                const sha256 = crypto.createHash('sha256').update(codeVerifier).digest();
+                const codeChallenge = sha256
+                    .toString('base64')
+                    .replace(/\+/g, '-')
+                    .replace(/\//g, '_')
+                    .replace(/=+$/, '');
+
+                vscode.window.showInformationMessage(`Client ID: ${codeChallenge}`);
+            }
+        });
     });
 
     context.subscriptions.push(disposable);
